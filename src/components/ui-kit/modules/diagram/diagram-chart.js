d92308dbbdaa54b59552706eac7eac81228fc9df
Vadim Manushin <vadimmanushin@yandex.ru>
2021-04-27 16:06:07 +0300

    added description number for donut chart

--- src/components/ui-kit/modules/diagram/diagram-chart.js
+++ src/components/ui-kit/modules/diagram/diagram-chart.js
@@ -2,7 +2,6 @@ class DiagramChart {
   constructor(rootElem, options) {
     this.rootElem = rootElem;
     this.options = this.getOptions(options);
-    console.log('inside constructor DiagramChart '+this.options.segments.satisfactory);
     this.initCanvas();
   }
   initCanvas() {
@@ -20,22 +19,22 @@ class DiagramChart {
     this.ctx.arc(centerX, centerY, radius, startAngle, endAngle, false);
     this.ctx.stroke();
   }
-  drawLine(ctx, startX, startY, endX, endY) {
-    ctx.beginPath();
-    ctx.strokeStyle = this.options.backgroundColor.background;
-    ctx.lineWidth = this.options.lineWidth;
-    ctx.moveTo(startX, startY);
-    ctx.lineTo(endX, endY);
-    ctx.stroke();
+  drawText(ctx, text, position, style) {
+    ctx.fillStyle = this.options.fontColor;
+    ctx.font = this.options.fontWeight + ' ' + style.fontSize + ' ' + this.options.font;
+    console.log(this.options.fontWeight + ' ' + style.fontSize + ' ' + this.options.font);
+    ctx.fillText(text, position.x, position.y);
   }
   drawChart() {
     let centerX = this.options.width / 2;
     let centerY = this.options.width / 2;
-    let radius = this.options.width / 2 - this.options.lineWidth;
-    let perfectAngle = (this.options.segments.perfect / this.options.totalNumber) * 2 * Math.PI;
-    let goodAngle = (this.options.segments.good / this.options.totalNumber) * 2 * Math.PI;
-    let poorAngle = (this.options.segments.poor / this.options.totalNumber) * 2 * Math.PI;
-    let satisfactoryAngle = (this.options.segments.satisfactory / this.options.totalNumber) * 2 * Math.PI;
+    let radius = (this.options.width > this.options.height)
+      ? (this.options.height / 2 - this.options.lineWidth)
+      : (this.options.width / 2 - this.options.lineWidth);
+    let perfectAngle = (this.options.segments.perfect / this.options.number) * 2 * Math.PI;
+    let goodAngle = (this.options.segments.good / this.options.number) * 2 * Math.PI;
+    let poorAngle = (this.options.segments.poor / this.options.number) * 2 * Math.PI;
+    let satisfactoryAngle = (this.options.segments.satisfactory / this.options.number) * 2 * Math.PI;
     if (poorAngle != 0) {
       this.drawArc(centerX, centerY, radius, this.options.initialAngle + this.options.delimeterAngle, this.options.initialAngle + poorAngle, this.options.backgroundColor.poor);
     }
@@ -50,17 +49,21 @@ class DiagramChart {
     if (perfectAngle != 0) {
       this.drawArc(centerX, centerY, radius, this.options.delimeterAngle + this.options.initialAngle + poorAngle + satisfactoryAngle + goodAngle, this.options.initialAngle + poorAngle + satisfactoryAngle + goodAngle + perfectAngle, this.options.backgroundColor.perfect);
     }
+    //this.drawText(this.ctx, this.options.number, { x: 60, y: 37 }, {fontSize: '24px',});
+    //this.drawText(this.ctx, this.options.label, { x: 80, y: 67 }, {fontSize: '12px',});
   }
   getOptions(options) {
     let width = options.width || 120;
     let height = options.height || 120;
-    let totalNumber = options.number || 260;
+    let font = options.font || 'Montserrat,sans-serif';
+    let number = options.number || 260;
     let label = options.label || "голосов";
     let defaultSegments = {
       perfect: 130, good: 65, satisfactory: 65, poor: 0,
     }
     let segments = options.segments || defaultSegments;
     let lineWidth = 4;
+    let fontWeight = 'bold';
     let delimeterAngle = lineWidth / (width / 2);
     let initialAngle = -Math.PI / 2;
     let backgroundColor = {
@@ -70,7 +73,9 @@ class DiagramChart {
       poor: '#919191',
       background: '#FFFFFF',
     };
-    return { width, height, totalNumber, label, segments, lineWidth, initialAngle, backgroundColor, delimeterAngle }
+    let defaultFontColor = '#BC9CFF';
+    let fontColor = options.fontColor || defaultFontColor;
+    return { width, height, number, label, segments, lineWidth, initialAngle, backgroundColor, delimeterAngle, font, fontColor, fontWeight }
   }
 }
 export default DiagramChart;
