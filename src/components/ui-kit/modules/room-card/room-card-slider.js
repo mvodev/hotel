c6744551fecb8c26464009a82f5f957cb31305e1
Vadim Manushin <vadimmanushin@yandex.ru>
2021-06-14 23:02:55 +0300

    added _ modificators for all private methods in room-card-slider.js

--- src/components/ui-kit/modules/room-card/room-card-slider.js
+++ src/components/ui-kit/modules/room-card/room-card-slider.js
@@ -1,28 +1,28 @@
 class RoomCardSlider {
- constructor(rootElem) {
+  constructor(rootElem) {
   this.rootElem = rootElem;
-  this.initSlider();
- }
- withArrows() {
+  this._initSlider();
+  }
+_withArrows() {
   if ($(this.rootElem).hasClass('room-card__slider_with-arrow')) {
-   return true;
+    return true;
   }
   return false;
- }
- initSlider() {
+}
+_initSlider() {
   $(this.rootElem).slick({
-   infinite: true,
-   dots: true,
-   dotsClass: 'room-card__slider-dots',
-   slidesToShow: 1,
-   slidesToScroll: 1,
-   useCSS: false,
-   arrows: this.withArrows(),
-   prevArrow:
+    infinite: true,
+    dots: true,
+    dotsClass: 'room-card__slider-dots',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    useCSS: false,
+    arrows: this._withArrows(),
+    prevArrow:
     '<button class="room-card__arrow room-card__arrow-left">expand_more</button>',
-   nextArrow:
+    nextArrow:
     '<button class="room-card__arrow room-card__arrow-right">expand_more</button>',
   });
- }
+}
 }
 export default RoomCardSlider;
--- src/components/ui-kit/modules/room-card/room-card.js
+++ src/components/ui-kit/modules/room-card/room-card.js
@@ -1,4 +1,4 @@
 import RoomCardSlider from './room-card-slider.js';
-document.querySelectorAll('.js-room-card__slider').forEach(elem=>{
+document.querySelectorAll('.js-room-card__slider').forEach(elem => {
  new RoomCardSlider(elem);
 });
