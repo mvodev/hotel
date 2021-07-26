7afbdf7f5c1751f3e9b4e9ad0fc319c4123e2bf5
unknown <vadimmanushin@yandex.ru>
2021-07-26 12:26:02 +0300

    added description for meta tag into all files of project

---
+++ docs/UIKit~details~landing~main~registration~search~signin.1344d2e203d33b89885b.js
@@ -0,0 +1,15 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["UIKit~details~landing~main~registration~search~signin"],{
+
+/***/ "./styles/style.scss":
+/*!***************************!*\
+  !*** ./styles/style.scss ***!
+  \***************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ })
+
+}]);
+//# sourceMappingURL=UIKit~details~landing~main~registration~search~signin.1344d2e203d33b89885b.js.map
