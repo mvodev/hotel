0b200168ea62e96cbf85f563decec152ea00b4af
unknown <vadimmanushin@yandex.ru>
2021-08-11 13:30:17 +0300

    deleted unused rules for typescript files from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -36,131 +36,127 @@ module.exports = {
   },
   optimization: {
     splitChunks: {
-    chunks: 'all'
+      chunks: 'all'
     }
   },
   devtool: 'source-map',
   plugins: [
     new HTMLWebpackPlugin(
-    {
-      template: './index.pug',
-      chunks: ['main'],
-    }
+      {
+        template: './index.pug',
+        chunks: ['main'],
+      }
     ),
     new HTMLWebpackPlugin(
-    {
-      template: './pages/ui-kit-page/ui-kit.pug',
-      filename: 'UIKit.html',
-      chunks: ['UIKit'],
-    }
+      {
+        template: './pages/ui-kit-page/ui-kit.pug',
+        filename: 'UIKit.html',
+        chunks: ['UIKit'],
+      }
     ),
     new HTMLWebpackPlugin(
-    {
+      {
         template: './pages/landing/landing.pug',
-      chunks: ['landing'],
-      filename: 'landing.html',
-    }
+        chunks: ['landing'],
+        filename: 'landing.html',
+      }
     ),
     new HTMLWebpackPlugin(
-    {
+      {
         template: './pages/details/details.pug',
-      chunks: ['details'],
-      filename: 'details.html',
-    }
+        chunks: ['details'],
+        filename: 'details.html',
+      }
     ),
     new HTMLWebpackPlugin(
-    {
+      {
         template: './pages/search/search.pug',
-      chunks: ['search'],
-      filename: 'search.html'
-    }
+        chunks: ['search'],
+        filename: 'search.html'
+      }
     ),
     new HTMLWebpackPlugin(
-    {
+      {
         template: './pages/registration/registration.pug',
-      chunks: ['registration'],
-      filename: 'registration.html'
-    }
+        chunks: ['registration'],
+        filename: 'registration.html'
+      }
     ),
     new HTMLWebpackPlugin(
-    {
+      {
         template: './pages/sign-in-page/sign-in-page.pug',
-      chunks: ['signin'],
-      filename: 'sign-in-page.html'
-    }
+        chunks: ['signin'],
+        filename: 'sign-in-page.html'
+      }
     ),
     new CleanWebpackPlugin(),
     new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery'
+      $: 'jquery',
+      jQuery: 'jquery'
     }),
     new MiniCssExtractPlugin({
-    filename: '[name].[hash].css'
+      filename: '[name].[hash].css'
     }),
     new CopyWebpackPlugin({
-    patterns: [
-      {
-      from: path.resolve(__dirname, 'src/assets/img/svg'),
-      to: path.resolve(__dirname, 'docs/assets/img/svg')
-      },
-      {
-      from: path.resolve(__dirname, 'src/assets/fonts'),
-      to: path.resolve(__dirname, 'docs/assets/fonts')
-      },
-      {
-      from: path.resolve(__dirname, 'src/assets/img/jpg'),
-      to: path.resolve(__dirname, 'docs/assets/img/jpg')
-      },
-      {
-      from: path.resolve(__dirname, 'src/assets/img/png'),
-      to: path.resolve(__dirname, 'docs/assets/img/png')
-      },
-      {
-      from: path.resolve(__dirname, 'src/assets/favicons'),
-      to: path.resolve(__dirname, 'docs/favicons')
-      },
-    ]
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/assets/img/svg'),
+          to: path.resolve(__dirname, 'docs/assets/img/svg')
+        },
+        {
+          from: path.resolve(__dirname, 'src/assets/fonts'),
+          to: path.resolve(__dirname, 'docs/assets/fonts')
+        },
+        {
+          from: path.resolve(__dirname, 'src/assets/img/jpg'),
+          to: path.resolve(__dirname, 'docs/assets/img/jpg')
+        },
+        {
+          from: path.resolve(__dirname, 'src/assets/img/png'),
+          to: path.resolve(__dirname, 'docs/assets/img/png')
+        },
+        {
+          from: path.resolve(__dirname, 'src/assets/favicons'),
+          to: path.resolve(__dirname, 'docs/favicons')
+        },
+      ]
     }),
     new ImageminPlugin({
-    test: /\.(jpe?g|png|gif|svg)$/i, 
-    pngquant: {
-      quality: '95-100'
-    }})
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      pngquant: {
+        quality: '95-100'
+      }
+    })
   ],
   module: {
     rules: [
-    {
-      test: /\.css$/,
-      use: cssLoaders()
-    },
-    {
-      test: /\.json$/,
-      use: ['json-loader'],
-    },
-    {
-      test: /\.pug$/,
-      use: ["pug-loader"],
-    },
-    {
-      test: /\.(png|jpg|svg|gif)$/,
-      use: ['file-loader']
-    },
-    {
-      test: /\.(ttf|woff|woff2|eot|otf)$/,
-      use: ['file-loader']
-    },
-    {
-      test: /\.s[ac]ss$/,
-      use: cssLoaders('sass-loader')
-    },
-    {
-      test: /\.tsx?$/,
-      use: 'ts-loader',
-      exclude: /node_modules/,
-    },
+      {
+        test: /\.css$/,
+        use: cssLoaders()
+      },
+      {
+        test: /\.json$/,
+        use: ['json-loader'],
+      },
+      {
+        test: /\.pug$/,
+        use: ["pug-loader"],
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot|otf)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: cssLoaders('sass-loader')
+      },
     ]
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.js'],
   },
 };
