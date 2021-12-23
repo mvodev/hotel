c00ec83f0918b61330d0d762aac41835f6c8183a
Vadim Manushin <vadimmanushin@yandex.ru>
2021-12-24 02:14:48 +0300

    burger.js imports removed into components

--- src/pages/landing/landing.js
+++ src/pages/landing/landing.js
@@ -4,3 +4,4 @@ import '@/pages/landing/landing.scss';
 import '@/components/burger/burger.js';
 import '@/components/dropdown/dropdown.js';
 import '@/components/list-links/sublinks.js';
+import '@/components/template/template.js';
--- src/pages/ui-kit-page/ui-kit.js
+++ src/pages/ui-kit-page/ui-kit.js
@@ -14,7 +14,7 @@ import '@/components/dropdown/dropdown.js';
 import '@/components/button-rate/star-rating.js';
 import '@/components/button-like/like-rating.js';
 import '@/components/list/expandable-cl.js';
-import '@/components/burger/burger.js';
+import '@/pages/headers-and-footers/headers-and-footers.js';
 import '@/components/room-card/room-card.js';
 import '@/components/range-slider/range-slider.js';
 import '@/components/list-links/sublinks.js';
