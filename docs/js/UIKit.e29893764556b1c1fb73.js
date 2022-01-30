03f00bc04fa57826bff0653886114eb1d6f5947f
Vadim Manushin <vadimmanushin@yandex.ru>
2022-01-30 11:52:58 +0300

    added doctype for index page

---
+++ docs/js/UIKit.e29893764556b1c1fb73.js
@@ -0,0 +1,385 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// install a JSONP callback for chunk loading
+/******/ 	function webpackJsonpCallback(data) {
+/******/ 		var chunkIds = data[0];
+/******/ 		var moreModules = data[1];
+/******/ 		var executeModules = data[2];
+/******/
+/******/ 		// add "moreModules" to the modules object,
+/******/ 		// then flag all "chunkIds" as loaded and fire callback
+/******/ 		var moduleId, chunkId, i = 0, resolves = [];
+/******/ 		for(;i < chunkIds.length; i++) {
+/******/ 			chunkId = chunkIds[i];
+/******/ 			if(Object.prototype.hasOwnProperty.call(installedChunks, chunkId) && installedChunks[chunkId]) {
+/******/ 				resolves.push(installedChunks[chunkId][0]);
+/******/ 			}
+/******/ 			installedChunks[chunkId] = 0;
+/******/ 		}
+/******/ 		for(moduleId in moreModules) {
+/******/ 			if(Object.prototype.hasOwnProperty.call(moreModules, moduleId)) {
+/******/ 				modules[moduleId] = moreModules[moduleId];
+/******/ 			}
+/******/ 		}
+/******/ 		if(parentJsonpFunction) parentJsonpFunction(data);
+/******/
+/******/ 		while(resolves.length) {
+/******/ 			resolves.shift()();
+/******/ 		}
+/******/
+/******/ 		// add entry modules from loaded chunk to deferred list
+/******/ 		deferredModules.push.apply(deferredModules, executeModules || []);
+/******/
+/******/ 		// run deferred modules when all chunks ready
+/******/ 		return checkDeferredModules();
+/******/ 	};
+/******/ 	function checkDeferredModules() {
+/******/ 		var result;
+/******/ 		for(var i = 0; i < deferredModules.length; i++) {
+/******/ 			var deferredModule = deferredModules[i];
+/******/ 			var fulfilled = true;
+/******/ 			for(var j = 1; j < deferredModule.length; j++) {
+/******/ 				var depId = deferredModule[j];
+/******/ 				if(installedChunks[depId] !== 0) fulfilled = false;
+/******/ 			}
+/******/ 			if(fulfilled) {
+/******/ 				deferredModules.splice(i--, 1);
+/******/ 				result = __webpack_require__(__webpack_require__.s = deferredModule[0]);
+/******/ 			}
+/******/ 		}
+/******/
+/******/ 		return result;
+/******/ 	}
+/******/
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// object to store loaded and loading chunks
+/******/ 	// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 	// Promise = chunk loading, 0 = chunk loaded
+/******/ 	var installedChunks = {
+/******/ 		"UIKit": 0
+/******/ 	};
+/******/
+/******/ 	var deferredModules = [];
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	var jsonpArray = window["webpackJsonp"] = window["webpackJsonp"] || [];
+/******/ 	var oldJsonpFunction = jsonpArray.push.bind(jsonpArray);
+/******/ 	jsonpArray.push = webpackJsonpCallback;
+/******/ 	jsonpArray = jsonpArray.slice();
+/******/ 	for(var i = 0; i < jsonpArray.length; i++) webpackJsonpCallback(jsonpArray[i]);
+/******/ 	var parentJsonpFunction = oldJsonpFunction;
+/******/
+/******/
+/******/ 	// add entry module to deferred list
+/******/ 	deferredModules.push(["./pages/ui-kit-page/ui-kit.js","vendors~UIKit~details~landing~search","vendors~UIKit~search","vendors~UIKit~registration","UIKit~details~landing~main~registration~search~signin","UIKit~details~landing~search","UIKit~search"]);
+/******/ 	// run deferred modules when ready
+/******/ 	return checkDeferredModules();
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./components/button-like/like-rating-class.js":
+/*!*****************************************************!*\
+  !*** ./components/button-like/like-rating-class.js ***!
+  \*****************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var autobind_decorator__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! autobind-decorator */ "../node_modules/autobind-decorator/lib/esm/index.js");
+var _class;
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+
+function _applyDecoratedDescriptor(target, property, decorators, descriptor, context) { var desc = {}; Object.keys(descriptor).forEach(function (key) { desc[key] = descriptor[key]; }); desc.enumerable = !!desc.enumerable; desc.configurable = !!desc.configurable; if ('value' in desc || desc.initializer) { desc.writable = true; } desc = decorators.slice().reverse().reduce(function (desc, decorator) { return decorator(target, property, desc) || desc; }, desc); if (context && desc.initializer !== void 0) { desc.value = desc.initializer ? desc.initializer.call(context) : void 0; desc.initializer = undefined; } if (desc.initializer === void 0) { Object.defineProperty(target, property, desc); desc = null; } return desc; }
+
+
+var LikeRating = (_class = /*#__PURE__*/function () {
+  function LikeRating(likeButton) {
+    _classCallCheck(this, LikeRating);
+
+    this.likeButton = likeButton;
+
+    this._getElems();
+
+    this._bindEvents();
+  }
+
+  _createClass(LikeRating, [{
+    key: "_bindEvents",
+    value: function _bindEvents() {
+      this.likeButton.addEventListener('click', this._handlelikeButtonClick);
+    }
+  }, {
+    key: "_getElems",
+    value: function _getElems() {
+      this.heart = this.likeButton.querySelector('.js-button-like__heart');
+      this.numberOfLikes = this.likeButton.querySelector('.js-button-like__number');
+    }
+  }, {
+    key: "_handlelikeButtonClick",
+    value: function _handlelikeButtonClick(event) {
+      if (event.target.parentElement.classList.contains('button-like_is-liked') || event.target.classList.contains('button-like_is-liked')) {
+        this.numberOfLikes.innerText--;
+        this.heart.innerText = 'favorite_border';
+        this.likeButton.classList.remove('button-like_is-liked');
+      } else {
+        this.numberOfLikes.innerText += 1;
+        this.likeButton.classList.add('button-like_is-liked');
+        this.heart.innerText = 'favorite';
+      }
+    }
+  }]);
+
+  return LikeRating;
+}(), (_applyDecoratedDescriptor(_class.prototype, "_handlelikeButtonClick", [autobind_decorator__WEBPACK_IMPORTED_MODULE_0__["default"]], Object.getOwnPropertyDescriptor(_class.prototype, "_handlelikeButtonClick"), _class.prototype)), _class);
+/* harmony default export */ __webpack_exports__["default"] = (LikeRating);
+
+/***/ }),
+
+/***/ "./components/button-like/like-rating.js":
+/*!***********************************************!*\
+  !*** ./components/button-like/like-rating.js ***!
+  \***********************************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _button_like_like_rating_class_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../button-like/like-rating-class.js */ "./components/button-like/like-rating-class.js");
+
+var likeButtons = document.querySelectorAll('.js-button-like');
+likeButtons.forEach(function (elem) {
+  return new _button_like_like_rating_class_js__WEBPACK_IMPORTED_MODULE_0__["default"](elem);
+});
+
+/***/ }),
+
+/***/ "./components/comment/comment.js":
+/*!***************************************!*\
+  !*** ./components/comment/comment.js ***!
+  \***************************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _components_button_like_like_rating_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @/components/button-like/like-rating.js */ "./components/button-like/like-rating.js");
+
+
+/***/ }),
+
+/***/ "./components/text-field/text-field-class.js":
+/*!***************************************************!*\
+  !*** ./components/text-field/text-field-class.js ***!
+  \***************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var cleave_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! cleave.js */ "../node_modules/cleave.js/dist/cleave-esm.js");
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+
+
+
+var TextField = /*#__PURE__*/function () {
+  function TextField(textFieldInput) {
+    _classCallCheck(this, TextField);
+
+    this.textFieldInput = textFieldInput;
+
+    this._init();
+  }
+
+  _createClass(TextField, [{
+    key: "_init",
+    value: function _init() {
+      new Cleave(this.textFieldInput, {
+        date: true,
+        delimiter: '.',
+        datePattern: ['d', 'm', 'Y']
+      });
+    }
+  }]);
+
+  return TextField;
+}();
+
+/* harmony default export */ __webpack_exports__["default"] = (TextField);
+
+/***/ }),
+
+/***/ "./components/text-field/text-field.js":
+/*!*********************************************!*\
+  !*** ./components/text-field/text-field.js ***!
+  \*********************************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _text_field_class_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./text-field-class.js */ "./components/text-field/text-field-class.js");
+
+document.querySelectorAll('.js-text-field__input-date').forEach(function (elem) {
+  return new _text_field_class_js__WEBPACK_IMPORTED_MODULE_0__["default"](elem);
+});
+
+/***/ }),
+
+/***/ "./pages/headers-and-footers/headers-and-footers.js":
+/*!**********************************************************!*\
+  !*** ./pages/headers-and-footers/headers-and-footers.js ***!
+  \**********************************************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _components_headers_header_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @/components/headers/header.js */ "./components/headers/header.js");
+
+
+/***/ }),
+
+/***/ "./pages/ui-kit-page/ui-kit-styles.scss":
+/*!**********************************************!*\
+  !*** ./pages/ui-kit-page/ui-kit-styles.scss ***!
+  \**********************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ "./pages/ui-kit-page/ui-kit.js":
+/*!*************************************!*\
+  !*** ./pages/ui-kit-page/ui-kit.js ***!
+  \*************************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* WEBPACK VAR INJECTION */(function($) {/* harmony import */ var _ui_kit_styles_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./ui-kit-styles.scss */ "./pages/ui-kit-page/ui-kit-styles.scss");
+/* harmony import */ var _ui_kit_styles_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_ui_kit_styles_scss__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _styles_style_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../styles/style.scss */ "./styles/style.scss");
+/* harmony import */ var _styles_style_scss__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_styles_style_scss__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _components_text_field_text_field_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @/components/text-field/text-field.js */ "./components/text-field/text-field.js");
+/* harmony import */ var _components_dropdown_dropdown_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @/components/dropdown/dropdown.js */ "./components/dropdown/dropdown.js");
+/* harmony import */ var _components_button_rate_star_rating_js__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @/components/button-rate/star-rating.js */ "./components/button-rate/star-rating.js");
+/* harmony import */ var _components_comment_comment_js__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! @/components/comment/comment.js */ "./components/comment/comment.js");
+/* harmony import */ var _components_list_expandable_cl_js__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! @/components/list/expandable-cl.js */ "./components/list/expandable-cl.js");
+/* harmony import */ var _pages_headers_and_footers_headers_and_footers_js__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! @/pages/headers-and-footers/headers-and-footers.js */ "./pages/headers-and-footers/headers-and-footers.js");
+/* harmony import */ var _components_room_card_room_card_js__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! @/components/room-card/room-card.js */ "./components/room-card/room-card.js");
+/* harmony import */ var _components_range_slider_range_slider_js__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! @/components/range-slider/range-slider.js */ "./components/range-slider/range-slider.js");
+/* harmony import */ var _components_pagination_pagination_js__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(/*! @/components/pagination/pagination.js */ "./components/pagination/pagination.js");
+
+
+
+
+
+
+
+
+
+
+
+$('.js-cards__datepicker').fsdDatepicker({
+  applyButton: true,
+  clearButton: true,
+  range: true,
+  dateFormat: 'dd M'
+});
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! jquery */ "../node_modules/jquery/dist/jquery.js")))
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=UIKit.e29893764556b1c1fb73.js.map
